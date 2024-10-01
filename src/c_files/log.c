448a6d763b9a34a6918873b9e73bad2ed36b1f37
Spatulox <M78stormtrooper@laposte.net>
2024-10-01 21:19:27 +0200

    add Log function

---
+++ src/c_files/log.c
@@ -0,0 +1,32 @@
+//
+// Created by M78st on 01/10/2024.
+//
+#include <time.h>
+#include <stdio.h>
+
+#include "../includes_h/log.h"
+
+void Log(char *string){
+    // Create the time string
+    time_t t = time(NULL);
+
+    struct tm* tm;
+    tm = localtime(&t);
+    char date[20];
+    strftime(date, 20, "%d/%m/%Y %H:%M:%S", tm);
+
+
+    fflush(stdout);
+
+    // Write the file
+    FILE *file = fopen("log.txt", "a+");
+    if (file == NULL) {
+        printf("[%s] : ERROR: Unable to open the log file\n", date);
+        return;
+    }
+    fprintf(file, "[%s] : %s\n", date, string);
+    fclose(file);
+    fflush(stdout);
+
+    printf("[%s] : %s\n", date, string);
+}
---
+++ src/includes_h/log.h
@@ -0,0 +1,10 @@
+//
+// Created by M78st on 01/10/2024.
+//
+
+#ifndef INTERPRETEURC_LOG_H
+#define INTERPRETEURC_LOG_H
+
+
+void Log(char *string);
+#endif //INTERPRETEURC_LOG_H
