4ac4447ec1805224ee0c801f0aa536ec01e9f1e7
Spatulox <M78stormtrooper@laposte.net>
2024-11-04 22:25:00 +0100

    Update struct to save variables

--- src/c_files/manage_list.c
+++ src/c_files/manage_list.c
@@ -14,9 +14,9 @@
 ListVariable* createVariableNode(TYPE type, Value value, char* varName) {
     ListVariable* node = malloc(sizeof(ListVariable));
     if (node) {
-        node->TYPE = type;
-        node->value = value;
-        node->varName = strdup(varName);  // Copie le nom de la variable
+        node->variable.TYPE = type;
+        node->variable.value = value;
+        node->variable.varName = strdup(varName);  // Copie le nom de la variable
         node->next = NULL;
     } else {
         Log("ERROR : Impossible to create a ListVariable node");
@@ -42,10 +42,10 @@ ListVariable* searchVariableInList(ListVariable* start, char* varName) {
     ListVariable* current = start;
 
     while (current != NULL) {
-        if (strcmp(current->varName, varName) == 0) {
+        if (strcmp(current->variable.varName, varName) == 0) {
             return current;
         }
-        current = (ListVariable*)current->next;
+        current = current->next;
     }
 
     return NULL;
@@ -56,7 +56,8 @@ ListVariable* searchVariableInList(ListVariable* start, char* varName) {
 void freeVariableList(ListVariable* head) {
     while (head) {
         ListVariable* temp = head;
-        head = (ListVariable *) head->next;
+        head = head->next;
+        free(temp->variable.varName);
         free(temp);
     }
 }
@@ -67,13 +68,13 @@ void printListsVar(ListVariable* variableList) {
     printf("Details of ListVariables:\n");
     ListVariable *currentVar = variableList;
     while (currentVar != NULL) {
-        printf("Name: %s, Type: ", currentVar->varName);
-        switch (currentVar->TYPE) {
+        printf("Name: %s, Type: ", currentVar->variable.varName);
+        switch (currentVar->variable.TYPE) {
             case INT:
-                printf("INT, Value: %d\n", currentVar->value.intValue);
+                printf("INT, Value: %d\n", currentVar->variable.value.intValue);
                 break;
             case CHAR:
-                printf("CHAR, Value: %s\n", currentVar->value.stringValue);
+                printf("CHAR, Value: %s\n", currentVar->variable.value.stringValue);
                 break;
             default:
                 printf("Unknown type\n");
