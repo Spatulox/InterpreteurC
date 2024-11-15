142fa304f9908c615d7eee5b0171dfabc2de9e31
Enzo <enzo.moy94@gmail.com>
2024-11-15 23:15:20 +0100

    move parser functions to a parser file

---
+++ src/c_files/parser.c
@@ -0,0 +1,144 @@
+/*////////////////////////////////////////////////////////*/
+//                                                        //   
+// Name: parser.c                                     //                     
+// Author: Enzo M                                         //  
+// Date: 15/11/2024                                          //  
+// Description: ...                                       //
+//                                                        //  
+/*////////////////////////////////////////////////////////*/
+
+#include "parser.h"
+
+ASTNode *create_number_node(const char* value) {
+    ASTNode *node = malloc(sizeof(ASTNode));
+    node->type = AST_NUMBER;
+
+
+    char* endptr;
+    double num = strtod(value, &endptr);
+
+    if (*endptr == '\0') {
+        // Ok
+        if (num == floor(num)) {
+            node->number.type = INT;
+            node->number.value.int_value = (int) num;
+        } else {
+            node->number.type = FLOAT;
+            node->number.value.float_value = (float) num;
+        }
+    } else {
+        printf("IMPOSSIBLE TO DETERMINE THE TYPE OF THE NUMBER (INT OF FLOAT)");
+        return 0;
+    }
+    return node;
+}
+
+ASTNode *create_binary_op_node(ASTNode *left, ASTNode *right, char op) {
+    ASTNode *node = malloc(sizeof(ASTNode));
+    node->type = AST_BINARY_OP;
+    node->binary_op.left = left;
+    node->binary_op.right = right;
+    node->binary_op.op = op;
+    return node;
+}
+
+// [{NUMBER, 1, SUITE}, {OPERATOR, +, SUITE}, {NUMBER, 2, SUITE}]
+ASTNode *parse_expression(Token **tokens) {
+    if(tokens == NULL){
+        return NULL;
+    }
+
+    if (*tokens && (*tokens)->type == VARIABLE && (*tokens)->nextToken && (*tokens)->nextToken->type == ASSIGNMENT) {
+        return parse_assignment(tokens);
+    }
+
+    ASTNode *node = parse_term(tokens);
+
+    while (*tokens != NULL && (*tokens)->type == OPERATOR &&
+           ((*tokens)->value[0] == '+' || (*tokens)->value[0] == '-')) {
+        char op = (*tokens)->value[0];
+        *tokens = (*tokens)->nextToken;
+        ASTNode *right = parse_term(tokens);
+        node = create_binary_op_node(node, right, op);
+    }
+
+    return node;
+}
+
+ASTNode *parse_term(Token **tokens) {
+    ASTNode *node = parse_primary(tokens);
+
+    while (*tokens != NULL && (*tokens)->type == OPERATOR &&
+           ((*tokens)->value[0] == '*' || (*tokens)->value[0] == '/' || (*tokens)->value[0] == '%')) {
+        char op = (*tokens)->value[0];
+        *tokens = (*tokens)->nextToken;
+        ASTNode *right = parse_primary(tokens);
+        node = create_binary_op_node(node, right, op);
+    }
+
+    return node;
+}
+
+
+ASTNode *parse_primary(Token **tokens) {
+    Token token = **tokens;
+
+    if (token.type == NUMBER) {
+        ASTNode *node = create_number_node(token.value);
+        *tokens = (*tokens)->nextToken;
+        return node;
+    } else if (token.type == VARIABLE) {
+        ASTNode *node = malloc(sizeof(ASTNode));
+        node->type = AST_VARIABLE;
+        node->variable.name = strdup(token.value);
+        *tokens = (*tokens)->nextToken;
+        return node;
+    } else if (token.type == PARENTHESIS_OPEN) {
+        *tokens = (*tokens)->nextToken;
+        ASTNode *node = parse_expression(tokens);
+        if ((*tokens)->type == PARENTHESIS_CLOSE) {
+            *tokens = (*tokens)->nextToken;
+        } else {
+            printf("Error: Missing closing parenthesis\n");
+            exit(1);
+        }
+        return node;
+    } else if (token.type == SCOPE_OPEN) {
+        ASTNode *node = malloc(sizeof(ASTNode));
+        node->type = AST_SCOPE_OPEN;
+        *tokens = (*tokens)->nextToken;
+        return node;
+    } else if (token.type == SCOPE_CLOSE) {
+        ASTNode *node = malloc(sizeof(ASTNode));
+        node->type = AST_SCOPE_CLOSE;
+        *tokens = (*tokens)->nextToken;
+        return node;
+    } else if (token.type == PRINT) {
+        *tokens = (*tokens)->nextToken;
+        ASTNode *node = malloc(sizeof(ASTNode));
+        node->type = AST_PRINT;
+        node->print.value = parse_expression(tokens);
+        return node;
+    }
+
+    printf("Error: Unrecognized token in parse_primary\n");
+    exit(1);
+}
+
+
+ASTNode *parse_assignment(Token **tokens) {
+    if ((*tokens)->type == VARIABLE) {
+        ASTNode *node = malloc(sizeof(ASTNode));
+        node->type = AST_ASSIGNMENT;
+        node->assignment.name = strdup((*tokens)->value);
+        *tokens = (*tokens)->nextToken;
+
+        if ((*tokens)->type == ASSIGNMENT) {
+            *tokens = (*tokens)->nextToken;
+            node->assignment.value = parse_expression(tokens);
+            return node;
+        }
+    }
+    printf("Error: Invalid assignment syntax\n");
+    exit(1);
+}
---
+++ src/includes_h/parser.h
@@ -0,0 +1,19 @@
+/*////////////////////////////////////////////////////////*/
+//                                                        //   
+// Name: parser.h                                     //                     
+// Author: Enzo M                                         //  
+// Date: 15/11/2024                                          //  
+// Description: ...                                       //
+//                                                        //  
+/*////////////////////////////////////////////////////////*/
+
+#ifndef INTERPRETEURC_PARSER_H
+#define INTERPRETEURC_PARSER_H
+
+#include "includes.h"
+#include "structs.h"
+#include "ast.h"
+#include "math.h"
+
+
+#endif //INTERPRETEURC_PARSER_H
